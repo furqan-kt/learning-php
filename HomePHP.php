9a88b4e38906f4a812262a87908e9cf1c2c3e5a4
unknown <mdfurqanbca@gmail.com>
2015-08-01 05:54:39 +0530

    HomePHP learn

---
+++ HomePHP.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        echo "my first php script!";
+        ?>
+  </body>
+</html>
+    
+<!--
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+-->
